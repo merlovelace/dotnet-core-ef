09ae921908f7bfc67d511c9e27c610631a826500
Merve Yolcu <104626041+merlovelace@users.noreply.github.com>
2024-04-17 23:14:35 +0300

    constructor

---
+++ Methods/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Math = Methods.Math;
+
+Math math = new Math();
+int increase = math.Increase(4,5);
+Console.WriteLine(increase);
+
+
+int increased2 = math.Increase(4,5,6,7,9);
+Console.WriteLine(increased2);
